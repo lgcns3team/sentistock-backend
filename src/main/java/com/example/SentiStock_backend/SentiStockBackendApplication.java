930477fe4e2f94a1a8e123dc397322d0496f1667
이경민 <rudals831@gmail.com>
2025-11-28 16:42:09 +0900

    first commit

---
+++ src/main/java/com/example/SentiStock_backend/SentiStockBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.SentiStock_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentiStockBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentiStockBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SentiStock_backend/SentiStockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SentiStock_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentiStockBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
